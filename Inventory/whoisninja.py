4acb32221392440f2b761aedc74b35f1162cb029
Rikunj Sindhwad <54503807+RikunjSindhwad@users.noreply.github.com>
2023-09-25 19:40:10 +0530

    Update whoisninja.py
    Improved

--- Inventory/whoisninja.py
+++ Inventory/whoisninja.py
@@ -1,6 +1,11 @@
-from reversewhois import * 
+#!/usr/local/bin/python
 import sys
 import argparse
+import time
+import requests
+import json
+url = "https://reverse-whois.whoisxmlapi.com/api/v2"
+headers = {"Content-Type": "application/json"}
 
 def read_file_lines(file_path):
     try:
@@ -30,46 +35,59 @@ def save_list_to_file(file_path, data):
 def getFeilds(input):
     match input:
         case "email":
-            return [Fields.admin_contact_email, Fields.registrant_contact_email, Fields.tech_contact_email,Fields.billing_contact_email]
+            return ["RegistrantContact.Email","AdminContact.Email","BillingContact.Email","TechContact.Email"]
         case "org":
-            return [Fields.admin_contact_organization,Fields.billing_contact_organization,Fields.registrant_contact_organization,Fields.tech_contact_organization]
+            return ["RegistrantContact.Organization","AdminContact.Organization","BillingContact.Organization","TechContact.Organization"]
         case "phone":
-            return [Fields.admin_contact_telephone,Fields.billing_contact_telephone,Fields.registrant_contact_telephone,Fields.tech_contact_telephone]
+            return ["RegistrantContact.Telephone","AdminContact.Telephone","BillingContact.Telephone","TechContact.Telephone"]
         case "domain":
-            return [Fields.domain_name]
+            return ["DomainName"]
             
-def reverseWhois(input,client,type):
+def reverseWhois(input,api_key,type):
     
     finalResult = []
-    
     fields = getFeilds(type)
     # print(fields)
-    print(f"[Start] Identifying Domains Through '{type}' against '{input}'",file=sys.stderr)
+    print(f"[Start] Identifying Domains Through '{type}': '{input}'",file=sys.stderr)
+    exectMatch = True
+    if "*" in input:
+        exectMatch = False
     for field in fields:
         advanced_terms = [{
             'field': field,
             'term': input
         }]
         try:
-            result = client.purchase(advanced_terms=advanced_terms,responseFormat=Client.JSON_FORMAT,searchType="historic",mode="purchase")
+            search_body = {
+                "apiKey": api_key,
+                "searchType": "historic",
+                "mode": "purchase",
+                'exactMatch': exectMatch,
+                "advancedSearchTerms":advanced_terms
+                    }
+            resp = requests.post(url,headers=headers,json=search_body)
+            domainList = json.loads(resp.text)["domainsList"]
         except Exception as e:
             print(f"Error: {e}",file=sys.stderr)
             sys.exit(1)
-        domainList = result.domains_list
         for domain in domainList:
-            finalResult.append(domain['domain_name'])    
-    # finalResult = list(set(finalResult))
-    print(f"[Done] Identified Total '{len(finalResult)}' Domains Through '{type}' against '{input}'",file=sys.stderr)
-    # print(finalResult[1])
+            finalResult.append(domain)
+    finalResult = list(set(finalResult))
+    print(f"[Done] Identified Total '{len(finalResult)}' Domains Through '{type}':'{input}'",file=sys.stderr)
     return finalResult
 
 parser = argparse.ArgumentParser(description="Reverse WHOIS lookup script by robensive (Rikunj Sindhwad)")
 parser.add_argument("--api_key", help="Your Reverse WHOIS API key (whoisxmlapi.com)",required=True)
 parser.add_argument("--output", help="Output file path", required=True)
-parser.add_argument("--org_list", help="File containing a list of organizations")
-parser.add_argument("--phone_list", help="File containing a list of phone numbers")
-parser.add_argument("--domain_list", help="File containing a list of domain names")
-parser.add_argument("--email_list", help="File containing a list of email addresses")
+parser.add_argument("--org_list", help="File containing a list of organizations (user * for wildcard)")
+parser.add_argument("--phone_list", help="File containing a list of phone numbers (user * for wildcard)")
+parser.add_argument("--domain_list", help="File containing a list of domain names (user * for wildcard)")
+parser.add_argument("--email_list", help="File containing a list of email addresses (user * for wildcard)")
+parser.add_argument("--email", help="Single email address (user * for wildcard)")
+parser.add_argument("--org", help="Single organization (user * for wildcard)")
+parser.add_argument("--phone", help="Single phone number (user * for wildcard)")
+parser.add_argument("--domain", help="Single domain name (user * for wildcard)")
+parser.add_argument("--sleep", help="Sleep time between each request (default 2 seconds)",type=int,default=2)
 
 # Parse the command line arguments
 args = parser.parse_args()
@@ -81,35 +99,53 @@ orgListfile = args.org_list if args.org_list else ""
 phoneListfile = args.phone_list if args.phone_list else ""
 domainListfile = args.domain_list if args.domain_list else ""
 emailListfile = args.email_list if args.email_list else ""
+sleep = args.sleep
 
-
-client = Client(api_key=apiKey)
 LastResult = []
 if orgListfile != "":
     orgList = read_file_lines(orgListfile)
     for org in orgList:
         org = org.strip()
-        result = reverseWhois(org,client,"org")
+        result = reverseWhois(org,apiKey,"org")
         LastResult.extend(result)
+        time.sleep(sleep)
 if phoneListfile != "":
     phoneList = read_file_lines(phoneListfile)
     for phone in phoneList:
         phone = phone.strip()
-        result = reverseWhois(phone,client,"phone")
+        result = reverseWhois(phone,apiKey,"phone")
         LastResult.extend(result)
+        time.sleep(sleep)
 if domainListfile != "":
     domainList = read_file_lines(domainListfile)
     for domain in domainList:
         domain = domain.strip()
-        result = reverseWhois(domain,client,"domain")
+        result = reverseWhois(domain,apiKey,"domain")
         LastResult.extend(result)
+        time.sleep(sleep)
 if emailListfile != "":
     emailList = read_file_lines(emailListfile)
     for email in emailList:
         email = email.strip()
-        result = reverseWhois(email,client,"email")
+        result = reverseWhois(email,apiKey,"email")
         LastResult.extend(result)
-
+        time.sleep(sleep)
+if args.email:
+    email = args.email.strip()
+    result = reverseWhois(email,apiKey,"email")
+    LastResult.extend(result)
+if args.org:
+    org = args.org.strip()
+    result = reverseWhois(org,apiKey,"org")
+    LastResult.extend(result)
+if args.phone:
+    phone = args.phone.strip()
+    result = reverseWhois(phone,apiKey,"phone")
+    LastResult.extend(result)
+if args.domain:
+    domain = args.domain.strip()
+    result = reverseWhois(domain,apiKey,"domain")
+    LastResult.extend(result)
 
 finalResult = list(dict.fromkeys(LastResult))
 save_list_to_file(output,finalResult)
