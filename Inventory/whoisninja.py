d8e17a4edc51029b581d8ebafbc5be35e74be6eb
Rikunj Sindhwad <54503807+RikunjSindhwad@users.noreply.github.com>
2023-10-05 13:21:37 +0530

    Update whoisninja.py
    Multiple Improvements

--- Inventory/whoisninja.py
+++ Inventory/whoisninja.py
@@ -5,7 +5,6 @@ import time
 import requests
 import json
 url = "https://reverse-whois.whoisxmlapi.com/api/v2"
-headers = {"Content-Type": "application/json"}
 
 def read_file_lines(file_path):
     try:
@@ -43,38 +42,58 @@ def getFeilds(input):
         case "domain":
             return ["DomainName"]
             
-def reverseWhois(input,api_key,type):
-    
+def reverseWhois(input, api_key, type, historic, retry_wait=5, max_retries=5):
     finalResult = []
-    fields = getFeilds(type)
-    # print(fields)
-    print(f"[Start] Identifying Domains Through '{type}': '{input}'",file=sys.stderr)
-    exectMatch = True
+    fields = getFeilds(type)  # Consider renaming this to getFields
+    default = "current"
+    if historic:
+        default = "historic"
+    print(f"[Start] Identifying {default} Domains Through '{type}': '{input}'", file=sys.stderr)
+    exactMatch = True  # Fixed the typo from exectMatch to exactMatch
     if "*" in input:
-        exectMatch = False
+        exactMatch = False
     for field in fields:
-        advanced_terms = [{
-            'field': field,
-            'term': input
-        }]
-        try:
-            search_body = {
-                "apiKey": api_key,
-                "searchType": "historic",
-                "mode": "purchase",
-                'exactMatch': exectMatch,
-                "advancedSearchTerms":advanced_terms
-                    }
-            resp = requests.post(url,headers=headers,json=search_body)
-            domainList = json.loads(resp.text)["domainsList"]
-        except Exception as e:
-            print(f"Error: {e}",file=sys.stderr)
+        advanced_terms = [{'field': field, 'term': input}]
+        retries = 0  # Initialize retry counter
+        while retries < max_retries:
+            try:
+                search_body = {
+                    "apiKey": api_key,
+                    "searchType": default,
+                    "mode": "purchase",
+                    'exactMatch': exactMatch,
+                    "advancedSearchTerms": advanced_terms
+                }
+                resp = requests.post(url, headers=headers, json=search_body)
+                if resp.status_code == 200:
+                    domainList = json.loads(resp.text)["domainsList"]
+                    break  # Break out of the retry loop if request is successful
+                elif resp.status_code == 429:
+                    print(f"[Info] Rate limited. Retrying in {retry_wait} seconds...", file=sys.stderr)
+                    time.sleep(retry_wait)  # Wait for X seconds before retrying
+                    retries += 1  # Increment retry counter
+                else:
+                    print(f"[Error] Unexpected status code {resp.status_code}. Exiting.", file=sys.stderr)
+                    sys.exit(1)
+            except Exception as e:
+                print(f"Error: {e}", file=sys.stderr)
+                sys.exit(1)
+        else:
+            print(f"Max retries reached. Exiting.", file=sys.stderr)
             sys.exit(1)
+            
         for domain in domainList:
             finalResult.append(domain)
     finalResult = list(set(finalResult))
-    print(f"[Done] Identified Total '{len(finalResult)}' Domains Through '{type}':'{input}'",file=sys.stderr)
+    print(f"[Done] Identified Total '{len(finalResult)}' Domains Through '{type}':'{input}'", file=sys.stderr)
     return finalResult
+
+def process_items(item_list, item_type, api_key, historic, retry_wait, max_retries):
+    for item in item_list:
+        item = item.strip()
+        result = reverseWhois(item, api_key, item_type, historic, retry_wait, max_retries)
+        return result
+
 
 parser = argparse.ArgumentParser(description="Reverse WHOIS lookup script by robensive (Rikunj Sindhwad)")
 parser.add_argument("--api_key", help="Your Reverse WHOIS API key (whoisxmlapi.com)",required=True)
@@ -88,9 +107,14 @@ parser.add_argument("--org", help="Single organization (user * for wildcard)")
 parser.add_argument("--phone", help="Single phone number (user * for wildcard)")
 parser.add_argument("--domain", help="Single domain name (user * for wildcard)")
 parser.add_argument("--sleep", help="Sleep time between each request (default 2 seconds)",type=int,default=2)
+parser.add_argument("--maxretry", help="Max retry on failure (default=5)",type=int,default=5)
+parser.add_argument("--historic", help="Get Historical domains too (default false)",action='store_true',default=False)
 
 # Parse the command line arguments
 args = parser.parse_args()
+headers = {"Content-Type": "application/json",
+           "X-Authentication-Token": args.api_key
+           }
 
 apiKey = args.api_key
 output = args.output
@@ -99,53 +123,34 @@ orgListfile = args.org_list if args.org_list else ""
 phoneListfile = args.phone_list if args.phone_list else ""
 domainListfile = args.domain_list if args.domain_list else ""
 emailListfile = args.email_list if args.email_list else ""
-sleep = args.sleep
 
+input_files = {
+    'org': orgListfile,
+    'phone': phoneListfile,
+    'domain': domainListfile,
+    'email': emailListfile
+}
+
+
+sleep = args.sleep
+historic = args.historic
 LastResult = []
-if orgListfile != "":
-    orgList = read_file_lines(orgListfile)
-    for org in orgList:
-        org = org.strip()
-        result = reverseWhois(org,apiKey,"org")
+for item_type, file_path in input_files.items():
+    if file_path:
+        item_list = read_file_lines(file_path)
+        result = process_items(item_list, item_type, apiKey, historic, sleep, args.sleep, args.maxretry)
         LastResult.extend(result)
-        time.sleep(sleep)
-if phoneListfile != "":
-    phoneList = read_file_lines(phoneListfile)
-    for phone in phoneList:
-        phone = phone.strip()
-        result = reverseWhois(phone,apiKey,"phone")
-        LastResult.extend(result)
-        time.sleep(sleep)
-if domainListfile != "":
-    domainList = read_file_lines(domainListfile)
-    for domain in domainList:
-        domain = domain.strip()
-        result = reverseWhois(domain,apiKey,"domain")
-        LastResult.extend(result)
-        time.sleep(sleep)
-if emailListfile != "":
-    emailList = read_file_lines(emailListfile)
-    for email in emailList:
-        email = email.strip()
-        result = reverseWhois(email,apiKey,"email")
-        LastResult.extend(result)
-        time.sleep(sleep)
-if args.email:
-    email = args.email.strip()
-    result = reverseWhois(email,apiKey,"email")
-    LastResult.extend(result)
-if args.org:
-    org = args.org.strip()
-    result = reverseWhois(org,apiKey,"org")
-    LastResult.extend(result)
-if args.phone:
-    phone = args.phone.strip()
-    result = reverseWhois(phone,apiKey,"phone")
-    LastResult.extend(result)
-if args.domain:
-    domain = args.domain.strip()
-    result = reverseWhois(domain,apiKey,"domain")
-    LastResult.extend(result)
+
+input_args = {
+    'org': args.org,
+    'phone': args.phone,
+    'domain': args.domain,
+    'email': args.email
+}
+
+for item_type, item_value in input_args.items():
+    if item_value:
+        process_items([item_value], item_type, apiKey, historic, args.sleep, args.maxretry)
 
 finalResult = list(dict.fromkeys(LastResult))
 save_list_to_file(output,finalResult)
